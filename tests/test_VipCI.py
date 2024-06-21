f4be75e8005c3e4612b350516b757aeec9b838f3
Hippolyte Blot <hippolytebl@gmail.com>
2024-06-21 17:59:36 +0200

    Adding tests and CI

--- tests/test_VipCI.py
+++ tests/test_VipCI.py
@@ -1,10 +1,13 @@
 import io
+from urllib.error import HTTPError
 import pytest
 from pathlib import *
 
+import pytest_mock
 from src.vip_client.utils import vip
-from src.vip_client.classes import VipLauncher
-from mocked_services import mock_vip_api, mock_pathlib, mock_os
+from src.vip_client.classes import VipCI
+from mocked_services import mock_vip_api, mock_girder_client, mock_pathlib, mock_os
+from FakeGirderClient import FakeGirderClient
 
 
 def get_properties(obj) -> dict:
@@ -26,16 +29,19 @@ def get_properties(obj) -> dict:
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_teardown_vip_launcher(request, mocker):
+    # Mock services
+    mock_vip_api(mocker, "LCModel/0.1")
+    mock_girder_client(mocker)
+    mock_pathlib(mocker)
+    mock_os(mocker)
+
     # Create a buffer file for the backup
     with open('tmp_data.json', 'w') as f:
         f.write('{}')
-    # Mock the VIP API
-    mock_vip_api(mocker, "LCModel/0.1")
-    mock_pathlib(mocker)
-    mock_os(mocker)
+    
     # Setup code before running the tests in the class
     print("Handshake with VIP")
-    VipLauncher.init(api_key="FAKE_KEY")
+    VipCI.init(vip_key="FAKE_KEY", girder_key="FAKE_KEY")
     print("Setup done")
 
 @pytest.fixture(scope="function", autouse=True)
@@ -59,27 +65,28 @@ def cleanup():
 )
 def test_run_and_finish(mocker, nb_runs, pipeline_id):
 
-    removed = False
-
-    def fake_exists(path):
-        if path == '/vip/Home/test-VipLauncher/OUTPUTS':
-            return True
-        if path == 'fake_value' and not removed:
-            return True
-        return False
+    FakeGirderClient.set_pipeline_id(pipeline_id)
+    wf_counter = 0
+    processing = True
     
-    def fake_delete_path(path):
-        nonlocal removed
-        removed = True
-        return True
+    def fake_init_exec(pipeline, name, inputValues, resultsLocation):
+        nonlocal wf_counter
+        wf_counter += 1
+        return f'workflow-{wf_counter}'
+             
+    def fake_execution_info(workflow_id):
+        nonlocal processing
+        if not processing:
+            return {'status': 'Finished', 'returnedFiles': [], 'startDate': 0}
+        processing -= 1
+        return {'status': 'Running', 'returnedFiles': [], 'startDate': 0}
     
-    mocker.patch("vip_client.utils.vip.exists").side_effect = fake_exists
-    mocker.patch("vip_client.utils.vip.delete_path").side_effect = fake_delete_path
-
-    removed = False
-
+    # Re patch the init_exec function to update the workflow counter
+    mocker.patch("vip_client.utils.vip.init_exec").side_effect = fake_init_exec
+    mocker.patch("vip_client.utils.vip.execution_info").side_effect = fake_execution_info
+    
     # Launch a Full Session Run
-    s = VipLauncher()
+    s = VipCI()
     s.pipeline_id = pipeline_id
     s.output_dir = PurePosixPath("/vip/Home/test-VipLauncher/OUTPUTS")
     s.input_settings = {
@@ -91,21 +98,15 @@ def test_run_and_finish(mocker, nb_runs, pipeline_id):
     s.run_session(nb_runs=nb_runs)
     # Check the Results
     assert s.workflows
-    assert len(s.workflows) == 1
+    assert len(s.workflows) == nb_runs
     for wid in s.workflows:
         assert s.workflows[wid]["status"] == "Finished"
     assert s.pipeline_id == pipeline_id
-    # Finish the Session
-    s.finish(timeout=50)
-    # Check Deletion
-    assert removed
-    for wid in s.workflows:
-        assert s.workflows[wid]["status"] == "Removed"
 
 @pytest.mark.parametrize(
     "backup_location, input_settings, pipeline_id, output_dir",
     [
-        ('vip', {
+        ('girder', {
             "zipped_folder": 'fake_value1',
             "basis_file": 'fake_value2',
             "signal_file": ['fake_value3', 'fake_value4'],
@@ -119,7 +120,7 @@ def test_run_and_finish(mocker, nb_runs, pipeline_id):
             "control_file": None
         }, "LCModel/0.1", PurePosixPath("/vip/Home/test-VipLauncher/OUTPUTS"),
         ),
-        ('vip', {
+        ('girder', {
             "zipped_folder": 'different_value1',
             "basis_file": 'different_value2',
             "signal_file": ['different_value3', 'different_value4'],
@@ -129,43 +130,20 @@ def test_run_and_finish(mocker, nb_runs, pipeline_id):
     ]
 )
 def test_backup(mocker, backup_location, input_settings, pipeline_id, output_dir):
+
+    VipCI._BACKUP_LOCATION = backup_location
         
-    removed = False
-
-    def fake_exists(path):
-        if path == '/vip/Home/test-VipLauncher/OUTPUTS':
-            return True
-        if path == 'fake_value' and not removed:
-            return True
-        return False
-    
-    def fake_delete_path(path):
-        nonlocal removed
-        removed = True
-        return True
-    
-    mocker.patch("vip_client.utils.vip.exists").side_effect = fake_exists
-    mocker.patch("vip_client.utils.vip.delete_path").side_effect = fake_delete_path
-
-    mock_vip_api(mocker, pipeline_id)
-    mock_pathlib(mocker)
-    mock_os(mocker)
-
-    VipLauncher._BACKUP_LOCATION = backup_location
     # Create session
-    s1 = VipLauncher()
-    s1.input_settings = input_settings
-    s1.pipeline_id = pipeline_id
+    s1 = VipCI(pipeline_id=pipeline_id, input_settings=input_settings)
     s1.output_dir = output_dir
     
-    # Return if backup is disabled
-    assert s1._save() is not (VipLauncher._BACKUP_LOCATION is None) # Return False if no backup location
+    assert s1._save() is not (VipCI._BACKUP_LOCATION is None) # Return False if no backup location
     
     # Load backup
-    s2 = VipLauncher(output_dir=s1.output_dir)
+    s2 = VipCI(output_dir=s1.output_dir)
     # Check parameters
     assert s2.output_dir == s1.output_dir
-    if VipLauncher._BACKUP_LOCATION is None:
+    if VipCI._BACKUP_LOCATION is None:
         assert not s2._load()
         assert s2.input_settings != s1.input_settings
         assert s2.pipeline_id != s1.pipeline_id
@@ -176,10 +154,10 @@ def test_backup(mocker, backup_location, input_settings, pipeline_id, output_dir
 
 def test_properties_interface(mocker):
 
-    VipLauncher._BACKUP_LOCATION = "vip"
+    VipCI._BACKUP_LOCATION = "girder"
 
     # Copy the first session
-    s = VipLauncher(output_dir=PurePosixPath("/vip/Home/test-VipLauncher/OUTPUTS"))
+    s = VipCI()
     s.input_settings = {
         "zipped_folder": 'fake_value1',
         "basis_file": 'fake_value2',
